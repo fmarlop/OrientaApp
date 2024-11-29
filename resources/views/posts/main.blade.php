155b3eb54a52dbde5d4063e81213f8778e5afa12
fmarlop <fmarlop94@gmail.com>
2024-11-29 13:00:58 +0100

    css animaciones

--- resources/views/posts/main.blade.php
+++ resources/views/posts/main.blade.php
@@ -4,8 +4,8 @@
 	{{-- <img src="{{ asset('storage/posts_images/EXaxsZ6LPuIzaYRkr4J9pGkw8g2l5ge0asYU1rK3.png') }}"  alt=""> --}}  {{-- función asset() busca dentro de /public --}}
 	<section>
 		<div>
-			<img src="{{ asset('storage/web_images/Camino3.png') }}" alt="">
-			<img src="{{ asset('storage/web_images/Mapa.jpg') }}" alt="">
+			<img src="{{ asset('storage/web_images/Camino.jpg') }}" alt="">
+			<img src="{{ asset('storage/web_images/Mapa.png') }}" alt="">
 			<img src="{{ asset('storage/web_images/Escalador.png') }}" alt="">
 		</div>
 		<h1>Siempre hay un camino</h1>
